a80f82e837f1695e3d019efda356a436ebee208d
nohero_ <serega1552@yandex.ru>
2022-06-02 16:32:41 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hw2111/Hw2111Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw2111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw2111Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hw2111/Hw2111ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw2111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
