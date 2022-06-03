373dab05a5f5f4b66013f54667784ba6c797dcaa
nohero_ <serega1552@yandex.ru>
2022-06-03 23:18:19 +0400

    spring hw complete

---
+++ src/main/java/pro/sky/java/course2/hw2111/services/StoreService.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course2.hw2111.services;
+
+
+import java.util.List;
+
+public interface StoreService {
+
+    void addItem(Integer id);
+
+    List<Integer> getBasket();
+}
---
+++ src/main/java/pro/sky/java/course2/hw2111/services/StoreServiceImpl.java
@@ -0,0 +1,32 @@
+package pro.sky.java.course2.hw2111.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.java.course2.hw2111.exceptions.WrongDataException;
+
+import java.util.List;
+
+@Service
+@SessionScope
+public class StoreServiceImpl implements StoreService {
+
+    private final List<Integer> basket;
+
+    public StoreServiceImpl(List<Integer> basket) {
+        this.basket = basket;
+    }
+
+    @Override
+    public void addItem(Integer id) {
+        if (id > 0) {
+            basket.add(id);
+        } else {
+            throw new WrongDataException();
+        }
+    }
+
+    @Override
+    public List<Integer> getBasket() {
+        return basket;
+    }
+}
